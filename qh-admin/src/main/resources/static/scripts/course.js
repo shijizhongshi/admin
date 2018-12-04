71b02fb9d591196ebbad16832f2da7a4a1d13cc6
guoyuxue <1490359123@qq.com>
2018-12-04 13:35:11 +0800

    js的修改

--- qh-admin/src/main/resources/static/scripts/course.js
+++ qh-admin/src/main/resources/static/scripts/course.js
@@ -1,7 +1,7 @@
 app.controller("CourseController", function($scope, $http){
        /*如果需要参数的时候就这届这样写,{"params": {"title":$scope.title}}, {'Content-Type': 'application/json;charset=UTF-8'}*/
       /*如果参数是对象类型的话, $scope.course, {'Content-Type': 'application/json;charset=UTF-8'}*/   
-	$http.get("/api/course/courseTypeList", $scope.course, {'Content-Type': 'application/json;charset=UTF-8'})
+	$http.get("/api/course/courseTypeList")
             .success(function(data){
                 if(data.status=="0"){
                 	$scope.courseType=data.data;
@@ -12,5 +12,4 @@ app.controller("CourseController", function($scope, $http){
             }).error(function(data){
             	alert(data.message);
             });
-    };
 });
