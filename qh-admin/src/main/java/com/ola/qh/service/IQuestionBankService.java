3a498073481eb402aff1ab88aac084e6d3effeee
Administrator <Administrator@BF-20180928MMZR>
2019-04-23 17:42:56 +0800

    java后台端导出表格完成

--- qh-admin/src/main/java/com/ola/qh/service/IQuestionBankService.java
+++ qh-admin/src/main/java/com/ola/qh/service/IQuestionBankService.java
@@ -2,7 +2,6 @@ package com.ola.qh.service;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ola.qh.entity.CourseLiveCheck;
